cc47c214ea3e96bcce91901e362a6d5cca87f69a
dayoung <clara@coinvest.kr>
2021-04-01 23:04:11 +0900

    class practice

--- src/components/Hello2.vue
+++ src/components/Hello2.vue
@@ -10,10 +10,38 @@ import { Component } from 'vue-property-decorator';
 
 @Component
 export default class Hello2 extends HelloWorld {
+  user = new UserAccount('Clara', 2)
+
   created() {
     console.log(this.name)
   }
+
+  mounted() {
+    console.log(this.user.getAdmin('da', 0))
+  }
 }
+
+// interface User {
+//   name: string
+//   id: number
+// }
+
+class UserAccount {
+  name: string
+  id: number
+
+  constructor(name: string, id: number) {
+    this.name = name
+    this.id = id
+  }
+
+  getAdmin(adminName: string, adminId: number): String {
+    this.name = adminName
+    this.id = adminId
+    return `${this.name} + ${this.id}`
+  }
+}
+
 </script>
 
 <style scoped>
