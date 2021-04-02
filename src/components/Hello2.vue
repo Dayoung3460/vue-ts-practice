3f9d5d51cf5f4c58c286cfe19d53a71801f824ff
dayoung <clara@coinvest.kr>
2021-04-02 18:02:17 +0900

    commit!

--- src/components/Hello2.vue
+++ src/components/Hello2.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     hello2 {{ sayHi('ccccc') }}
+    <button @click="getArea({ kind: 'triangle', x: 3, y: 4})">getArea</button>
+    <span>{{ greeter(user3) }}</span>
+
   </div>
 </template>
 
@@ -10,7 +13,12 @@ import { Component } from 'vue-property-decorator';
 
 @Component
 export default class Hello2 extends HelloWorld {
+
+
   user = new UserAccount('Clara', 2)
+  user2 = { firstName: 'Jane', lastName: 'User'}
+  user3 = new Student('Jane', 'M', 'User')
+
 
   created() {
     console.log(this.name)
@@ -18,13 +26,18 @@ export default class Hello2 extends HelloWorld {
 
   mounted() {
     console.log(this.user.getAdmin('da', 0))
+    logText<LengthWise>({ length: 10, value: 'hi' })
   }
-}
 
-// interface User {
-//   name: string
-//   id: number
-// }
+  getArea(obj: Shape) {
+    area(obj)
+  }
+
+  greeter(person: Person) {
+    return "Hello, " + person.firstName + ' ' + person.lastName
+  }
+
+}
 
 class UserAccount {
   name: string
@@ -41,6 +54,51 @@ class UserAccount {
     return `${this.name} + ${this.id}`
   }
 }
+
+interface LengthWise {
+  length: number
+  value: string
+}
+
+function logText<T extends LengthWise>(text: T): T {
+  console.log(text.length)
+  return text
+}
+
+type Shape = { kind: "circle"; radius: number } | { kind: "square"; x: number } | { kind: "triangle"; x: number; y: number }
+function area(s: Shape): number {
+  if(s.kind === 'circle') {
+    console.log('shape', Math.PI * s.radius * s.radius)
+    return Math.PI * s.radius * s.radius
+  } else if(s.kind === 'square') {
+    console.log('shape2', s.x * s.x)
+    return s.x * s.x
+  } else {
+    console.log('shape3', (s.x * s.y) / 2)
+    return (s.x * s.y) / 2
+  }
+}
+
+interface Person {
+  firstName: string
+  lastName: string
+}
+
+class Student {
+  fullName: string
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName
+  }
+}
+
+
+
+
+
+
+
+
+
 
 </script>
 
